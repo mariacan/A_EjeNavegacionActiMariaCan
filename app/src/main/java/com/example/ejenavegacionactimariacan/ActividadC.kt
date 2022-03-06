15d296215c65b847563ba20d4f422ce1799a6326
mariacan <canbalammariajose@gmail.com>
2022-03-05 23:45:58 -0500

    Navegacion

---
+++ app/src/main/java/com/example/ejenavegacionactimariacan/ActividadC.kt
@@ -0,0 +1,13 @@
+package com.example.ejenavegacionactimariacan
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class ActividadC : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actividad_c)
+
+    }
+}
---
+++ app/src/main/java/com/example/ejenavegacionactimariacan/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.ejenavegacionactimariacan
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val boton = findViewById<Button>(R.id.boton)
+        boton.setOnClickListener {
+            var intent = Intent(this,ActividadB::class.java)
+            startActivity(intent)
+        }
+    }
+}
